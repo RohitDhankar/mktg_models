c835b9d977620cbfc71b1b728b55348533b9c84e
RohitDhankar <dhankar.rohit@gmail.com>
2021-06-05 23:23:17 +0530

    init

---
+++ source_kaggle.py
@@ -0,0 +1,60 @@
+## SOURCE -- Multitouch Attribution Modelling == https://www.kaggle.com/hughhuyton/multitouch-attribution-modelling
+## Data Source == https://www.dropbox.com/sh/m5d8u7inolw32xu/AADuHNcGToa7ajTcF_JjEkcca?dl=0
+
+#Select Distinct 
+#Select Col_1 and Drop Duplicates 
+#df.loc[:, ['col_1']].drop_duplicates()
+
+#----------------------------------------#----------------------------------------
+#SQL - 
+# SELECT TOP number col1, col2, ... FROM table
+# or
+# SELECT col1, col2, ... FROM table LIMIT number
+
+#PANDAS
+#df.loc[:, ['col1', 'col2']].head(5)
+
+#----------------------------------------#----------------------------------------
+#SQL 
+#SELECT MIN(col) FROM table
+
+#PANDAS
+#df.loc[:, ['col1']].min()
+#df.loc[:, ['col1']].max()
+#df.loc[:, ['col1']].count()
+#df.loc[:, ['col1']].mean()
+#df.loc[:, ['col1']].sum()
+
+#----------------------------------------#----------------------------------------
+#SQL 
+#SELECT col1, col2, ... FROM table WHERE col3>=1000
+
+#PANDAS
+#df.loc[df['col3'] >= 1000, ['col1', 'col2']]
+# Select records from col1 and col2 , where the records in col3 have values >= 1000
+
+#----------------------------------------#----------------------------------------
+#SQL 
+
+
+#PANDAS
+
+
+
+#----------------------------------------#----------------------------------------
+#SQL 
+
+
+#PANDAS
+
+#----------------------------------------#----------------------------------------
+#SQL 
+
+
+#PANDAS
+
+#----------------------------------------#----------------------------------------
+#SQL 
+
+
+#PANDAS
